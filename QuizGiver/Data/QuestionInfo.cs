ae4dae782d83c9b511f31db76f31f8a0e47b1fdf
Theo <theodor.salla@gmail.com>
2022-09-17 21:34:10 +0200

    added connection to sql server and created database

---
+++ QuizGiver/Data/QuestionInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizGiver
+{
+    public class QuestionInfo
+    {
+         public int Id { get; set; }
+         public string Question { get; set; }
+         public string CorrectAnswer { get; set; }
+         public string IncorrectAnswer { get; set; }
+         public string Difficulty { get; set; }
+         public string Category { get; set; }
+    }
+}
