01c36f539329efe4990dc10af7ac6dd362725901
Theo <theodor.salla@gmail.com>
2022-11-12 18:28:06 +0100

    connected to db to get questions

--- QuizGiver/Controllers/QuestionController.cs
+++ QuizGiver/Controllers/QuestionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using QuizGiver.Models;
+using QuizGiver.Repository;
 using RestClientLib;
 
 namespace QuizGiver.Controllers
@@ -13,33 +14,50 @@ namespace QuizGiver.Controllers
     public class QuestionController : ControllerBase
     {
 
-        private readonly IJsonToModel questions;
-        private readonly Token token;
+        private readonly IJsonToModel _questions;
+        private readonly Token _token;
+        private readonly IQuestionRepository _questionRepository;
+        private bool finish;
 
-        public QuestionController(IJsonToModel questions, Token token)
+        public QuestionController(IJsonToModel questions, Token token, IQuestionRepository questionRepository)
         {
-            this.questions = questions;
-            this.token = token;
+            this._questions = questions;
+            this._token = token;
+            this._questionRepository = questionRepository;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetQuestion([FromQuery] Question q)
-        {       
-            if(Enum.TryParse(q.Category, out Category category) && Enum.TryParse(q.Difficulty, out Difficulty difficulty ))
+        {
+            if(finish)
+            {
+                return RedirectToAction(actionName: "GetQuestionFromDb");
+            }
+            
+            if (Enum.TryParse(q.Category, out Category category) && Enum.TryParse(q.Difficulty, out Difficulty difficulty ))
             {
                 int count = q.Count ?? 10;
-                RestClientLib.Questions listOfQuestions = new();
-                listOfQuestions = await this.questions.GetQuestions(category, difficulty, count, token.SessionToken);
+                Questions listOfQuestions = await this._questions.GetQuestions(category, difficulty, count, _token.SessionToken);
                 if (listOfQuestions.ResponseCode == 0)
                 {
                     return Ok(listOfQuestions);
                 }
                 else if (listOfQuestions.ResponseCode == 4)
                 {
-                    return Ok("No more questions");
+                    finish = true;
+                    Console.WriteLine("No more question");
+                    return RedirectToAction(actionName: "GetQuestionFromDb");
                 }
             }
             return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("db")]
+        public async Task<IActionResult> GetQuestionFromDb()
+        {
+            var questions = await _questionRepository.GetAllQuestionAsync();
+            return Ok(questions);
         }
     }
 }
