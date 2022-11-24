4777103b0a713b0f8be32580b638e477c68eae7a
Theo <theodor.salla@gmail.com>
2022-11-24 01:10:01 +0100

    fixed useEffect to render once

--- quiz_front/src/components/Questions.js
+++ quiz_front/src/components/Questions.js
@@ -1,88 +1,66 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { Button, Paper, Container, Box } from '@mui/material';
+import { useState, useEffect, useRef } from 'react'
+import { Button, Container } from '@mui/material';
 
-
-class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
-}
 
 
 const Questions = (props) => {
-  const [questions, setQuestions] = useState([{
-    "correctAnswer":""
-  },
-  {
-    "correctAnswer":""
-  }])
-  const [question, setQuestion] = useState("")
-  const [state, setState] = useState();
+    const effectRan = useRef(false);
+
+    const [questions, setQuestions] = useState([{
+        "correctAnswer": ""
+    },
+    {
+        "correctAnswer": ""
+    }])
+    const [question, setQuestion] = useState("")
+    const [state, setState] = useState();
 
 
-  useEffect(() => {
-    const getTasks = async () => {
-      const questionsFromServer = await fetchQuestions();
-      setQuestions(questionsFromServer);
-      setQuestion(questionsFromServer[0].question)
+    useEffect(() => {
+        if (effectRan.current === false) {
+            const getTasks = async () => {
+                const questionsFromServer = await fetchQuestions();
+                setQuestions(questionsFromServer);
+                setQuestion(questionsFromServer[0].question)
+            }
+
+            getTasks();
+            console.log(questions);
+            console.log('Getting questions....');
+            effectRan.current = true;
+
+        }
+
+    }, [])
+
+    // Fetch Questions
+    const fetchQuestions = async () => {
+        const res = await fetch(`https://localhost:7112/api/question?difficulty=hard&category=${props.category}&count=10`)
+        const data = await res.json()
+        return data;
     }
 
-    getTasks();
-    console.log(questions);
-    console.log('Getting questions....');
+    console.log("HELLO HELLO HELLO!!!");
 
-  }, [])
+    return (
+        <div hidden={props.hidden}>
+            <Button onClick={() => console.log(question)}>Click here</Button>
 
-  // Fetch Questions
-  const fetchQuestions = async () => {
-    const res = await fetch(`https://localhost:7112/api/question?difficulty=medium&category=${props.category}&count=5`)
-    const data = await res.json()
-    return data;
-  }
+            <Container className='categoryGroup' maxWidth="md">
+                <h1>{question}</h1>
+                <div className='question' >
+                    {questions.map(q => {
+                        return <div className='btn1'> <Button size='large' color='success' variant='contained' key={q.question}>{questions[0].correctAnswer}</Button> </div>
+                    })}
+                    <h1>{questions[0].correctAnswer}</h1>
 
-  console.log("HELLO HELLO HELLO!!!");
+                    <Button onClick={props.back}>Go back</Button>
+                </div>
 
-  return (
-    <div hidden={props.hidden}>
-      <Button onClick={() => console.log(question)}>Click here</Button>
-
-      <Container className='categoryGroup' maxWidth="md">
-        <h1>{question}</h1>
-        <div className='question' >
-        {questions.map(q => {
-          return  <div className='btn1'> <Button size='large' color='success' variant='contained' key={q.question}>{questions[0].correctAnswer}</Button> </div>
-        })}
-        <h1>{questions[0].correctAnswer}</h1>
-
-       <Button onClick={props.back}>Go back</Button>
-
-    
-
-          {/* <div className='btn1'>
-            <Button size='large' color='success' variant='contained'>ABCDEEF</Button>
-          </div>
-          <div className='btn1'>
-            <Button size='large' color='success' variant='contained'>BBBBBBB</Button>
-          </div>
-          <div className='btn1'>
-            <Button size='large' color='success' variant='contained'>Cddd</Button>
-          </div>
-          <div className='btn1'>
-            <Button size='large' color='success' variant='contained'>Dgg</Button>
-          </div> */}
+            </Container>
         </div>
-        {/* {questions.map(q => {
-          return <h1 key={q.question}>{questions[0].correctAnswer}</h1>
-        })}
-        {questions.map(q => {
-          return <h1 key={q.question}>{questions[0].incorrectAnswers}</h1>
-        })} */}
-
-      </Container>
-    </div>
-  )
+    )
 }
 
 export default Questions
