1d0136e63f9e82a7e652984b6d8e0100035c506f
Theo <theodor.salla@gmail.com>
2024-07-24 09:30:43 +0200

    add log output

--- QuizGiver/Controllers/QuestionController.cs
+++ QuizGiver/Controllers/QuestionController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using QuizGiver.Models;
 using QuizGiver.Repository;
 using RestClientLib;
+using Microsoft.Extensions.Logging;
 
 namespace QuizGiver.Controllers
 {
@@ -12,28 +13,39 @@ namespace QuizGiver.Controllers
         private readonly IJsonToModel _questions;
         private readonly IQuestionRepository _questionRepository;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<QuestionController> _logger;
 
-        public QuestionController(IJsonToModel questions, IQuestionRepository questionRepository, IHttpClientFactory httpClientFactory)
+        public QuestionController(IJsonToModel questions, IQuestionRepository questionRepository, IHttpClientFactory httpClientFactory, ILogger<QuestionController> logger)
         {
+            Console.WriteLine("QuestionController constructor called");
             this._httpClientFactory = httpClientFactory;
             this._questions = questions;
             this._questionRepository = questionRepository;
+            this._logger = logger;
         }
         [HttpGet]
         public async Task<IActionResult> GetQuestion([FromQuery] Question q)
         {
+            _logger.LogInformation("GetQuestion called with {Category} and {Difficulty}", q.Category, q.Difficulty);
+
+
             if (Enum.TryParse(q.Category, out Category category) && Enum.TryParse(q.Difficulty, out Difficulty difficulty))
             {
+                _logger.LogInformation("Category and Difficulty parsed successfully");
                 int count = (q.Count > 0) ? q.Count : 10;
                 Questions listOfQuestions = await this._questions.GetQuestions(_httpClientFactory.CreateClient(), category, difficulty, count, HttpContext.Request.Cookies["session_token"]!);
+                _logger.LogInformation("Response code: {ResponseCode}", listOfQuestions.ResponseCode);
                 if (listOfQuestions.ResponseCode == 0)
                 {
+                    _logger.LogInformation("Getting questions from API");
                     return Ok
                     (listOfQuestions.Results);
                 }
-                else if 
+                else if
                 (listOfQuestions.ResponseCode == 4)
                 {
+                    _logger.LogInformation("Session token expired or invalid quiz category");
+                    _logger.LogInformation("Getting questions from Database");
                     return RedirectToAction(actionName: "GetQuestionFromDbBasedOnCategory");
                 }
             }
